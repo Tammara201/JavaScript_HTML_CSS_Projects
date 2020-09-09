6a6e9849bb7f33282db26ead666da31d28a3ff02
Tammara201 <69922874+Tammara201@users.noreply.github.com>
2020-09-09 15:54:02 -0700

    Add files via upload
    Updates for step 116

--- Basic_JavaScript_Projects/JS/Basic_JavaScript_5.js
+++ Basic_JavaScript_Projects/JS/Basic_JavaScript_5.js
@@ -15,3 +15,51 @@ function nan_Function() {
 document.write(5E310); //display 'infinity'
 
 document.write(-5E310);  //display '-infinity'
+
+document.write(15 > 7);  //display 'true'
+
+document.write(15 < 7);  //display 'false'
+
+console.log(7<5);
+
+document.write("25" + 0);  //display '250'
+
+document.write(10+5 > 7);  //display 'true'
+
+document.write(10+5 < 7);  //display 'false'
+
+document.write(10+5 == 12);  //display 'true'
+
+document.write(10+5 == 7);  //display 'false'
+
+X = 13;
+Y = 13;
+document.write(X === Y);  //display 'true'
+
+X = 13;
+Y = '13';
+document.write(X === Y);  //display 'false'
+
+X = 13;
+Y = 'Fourteen';
+document.write(X === Y);  //display 'false'
+
+X = 13;
+Y = 29;
+document.write(X === Y);  //display 'false'
+
+document.write(10>2 && 12>10);  //display 'true'
+
+document.write(10>2 && 10>12);  //display 'false'
+
+document.write(10>12 || 12>10);  //display 'true'
+
+document.write(10<2 || 10>12);  //display 'false'
+
+function not_Function() { //display false on button click
+    document.getElementById("Not").innerHTML = !(25>17); //display 'false'
+}
+
+function notTrue_Function() { //display true on button tip
+    document.getElementById("NotTrue").innerHTML = !(2>10); //display 'true'
+}
