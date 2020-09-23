81b3f6d48528bf60b926be51bf633a82c2fcd200
Tammara201 <69922874+Tammara201@users.noreply.github.com>
2020-09-23 12:47:28 -0700

    Add files via upload
    changes with auto slideshow

--- JavaScript_Projects/Bootstrap4_Project/Bootstrap4_Project/js/cinema.js
+++ JavaScript_Projects/Bootstrap4_Project/Bootstrap4_Project/js/cinema.js
@@ -1,9 +1,11 @@
+//adds fade out on opening of page
 document.body.className += ' fade-out';
 
 $(function(){
     $('body').removeClass('fade-out')
 });
 
+//validates first name is added to movie deals form
 function validateForm() {
     var x = document.forms["myForm"]["firstname"].value;
     if (x == "") {
@@ -11,3 +13,24 @@ function validateForm() {
       return false;
     }
   }
+
+  //login form opens when Login here clicked
+  function openForm() {
+    document.getElementById("contactForm").style.display = "block";
+  }
+  
+  function closeForm() {
+    document.getElementById("contactForm").style.display = "none";
+  }
+
+  //movie slideshow auto play
+  $("#slideshow > div:gt(0)").hide();
+
+setInterval(function() { 
+  $('#slideshow > div:first')
+    .fadeOut(1000)
+    .next()
+    .fadeIn(1000)
+    .end()    
+    .appendTo('#slideshow');
+},  3000);
